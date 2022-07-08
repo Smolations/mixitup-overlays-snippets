8cbd4d05b879acd444ee740e697b1e63858ae71b
Chris Smola <callmesmola@gmail.com>
2022-07-08 16:07:24 -0600

    refactors Panel; broken into pieces so drop shadow applies appropriately

--- browser-sources/components/Panel/Panel.mjs
+++ browser-sources/components/Panel/Panel.mjs
@@ -15,19 +15,16 @@ export default class Panel {
     '--x-offset': this.xFrameWidth,
     '--y-offset-calc': `calc(100% - ${this.yFrameWidth})`,
     '--x-offset-calc': `calc(100% - ${this.xFrameWidth})`,
-    '--logo-width': this.logoWidth,
-    '--logo-unused-width': `calc(100% - ${this.logoWidth})`,
     '--logo-unused-width-half': `calc((100% - ${this.logoWidth}) / 2)`,
-    '--logo-offset': this.logoOffset,
     '--logo-offset-2x': `calc(2 * ${this.logoOffset})`,
     '--logo-total-height': `calc(${this.logoHeight} + 2 * ${this.logoOffset})`,
     '--border-radius': '3px',
+    '--drop-shadow': 'drop-shadow(0 -4px 8px #000)',
   };
 
   panelCommonCss = {
     boxSizing: 'border-box',
     background: 'repeat url("./img/rusty-iron-plate-bg.jpg")',
-    borderRadius: 'var(--border-radius)',
   };
 
 
@@ -42,7 +39,7 @@ export default class Panel {
   $getPanel() {
     const { height, width } = this;
 
-    const $panel = $('<div>')
+    return $('<div>')
       .addClass('Panel')
       .css({
         ...this.cssVars,
@@ -51,110 +48,52 @@ export default class Panel {
         height,
         width,
         padding: this.frameWidth,
-        filter: 'drop-shadow(0 -4px 8px #000)',
+        borderRadius: 'var(--border-radius)',
 
         // testing
         background: 'white',
         color: 'white',
       });
-
-
-
-
-    return $panel;
   }
 
-  getPanelFrameClipPath() {
-    const aroundBoxAtTopLeft = '0 0, 100% 0, 100% 100%, 0 100%, 0 0,';
-
-    // can't go same direction as initial path in order
-    // to clip out just the frame
-    const topLeftToBottomLeft = `
-      var(--x-offset) var(--y-offset),
-      var(--x-offset) var(--y-offset-calc),
-    `;
-
-    // left to right
-    const logoLip = `
-      calc(var(--logo-unused-width-half) - var(--logo-offset-2x)) var(--y-offset-calc),
-      calc(var(--logo-unused-width-half)) calc(100% - var(--logo-total-height)),
-      calc(100% - var(--logo-unused-width-half)) calc(100% - var(--logo-total-height)),
-      calc(100% - (var(--logo-unused-width-half) - var(--logo-offset-2x))) var(--y-offset-calc),
-    `;
-
-    const bottomRightToTopLeft = `
-      var(--x-offset-calc) var(--y-offset-calc),
-      var(--x-offset-calc) var(--y-offset),
-      var(--x-offset) var(--y-offset)
-    `;
-
-    return `polygon(
-      ${aroundBoxAtTopLeft}
-      ${topLeftToBottomLeft}
-      ${logoLip}
-      ${bottomRightToTopLeft}
-    )`;
-  }
-
-  $getPanelFrame() {
-    const { frameWidth, panelCommonCss: { borderRadius } } = this;
-    const clipPath = this.getPanelFrameClipPath();
-
-    const $frame = $('<div>')
-      .addClass('Panel--frame')
+  $getFrameTop() {
+    return $('<div>')
+      .addClass('Panel--frame-top')
       .css({
         ...this.panelCommonCss,
         position: 'absolute',
-        top: 0,
-        bottom: 0,
-        right: 0,
         left: 0,
-        clipPath,
+        top: 0,
+        width: '100%',
+        height: 'calc(100% - var(--y-offset))',
+        borderRadius: 'var(--border-radius) var(--border-radius) 0 0',
+        clipPath: `polygon(
+          0 0,
+          100% 0,
+          100% 100%,
+          var(--x-offset-calc) 100%,
+          var(--x-offset-calc) var(--y-offset),
+          var(--x-offset) var(--y-offset),
+          var(--x-offset) 100%,
+          0 100%,
+          0 0
+        )`,
       });
-
-
-    // for masking, was only able to get default alpha to work. take black/white
-    // image into gimp, create a layer mask from a greyscale copy of the image
-    // (option when creating the mask) and then check "invert". the shorthand
-    // `mask` property didn't work either, only webkit-prefixed props
-    const $logo = $('<img>')
-      .attr('src', './img/primary2.png')
-      .css({
-        boxSizing: 'border-box',
-        position: 'absolute',
-        bottom: this.logoOffset,
-        left: '50%',
-        transform: 'translateX(-50%)',
-        width: this.logoWidth, // height then ~21px
-        '-webkit-mask-image': 'url("./img/mask_rust_alpha_black.png")',
-        '-webkit-mask-size': '100%',
-        filter: 'drop-shadow(0px 0px 3px #000) brightness(0.9)',
-      })
-      .appendTo($frame);
-
-    return $frame;
   }
 
-  attachFrameShadowElements() {
-    const $wrapper = $('<div>')
+  $getFrameBottom() {
+    const $logo = this.$getFrameLogo();
+
+    return $('<div>')
+      .addClass('Panel--frame-bottom')
       .css({
-        boxSizing: 'border-box',
+        ...this.panelCommonCss,
         position: 'absolute',
-        bottom: 0,
         left: 0,
+        bottom: 0,
         width: '100%',
         height: 'var(--logo-total-height)',
-        filter: 'drop-shadow(0 -4px 8px #000)',
-      });
-
-    // clipped element needs a wrapper to apply drop shadow. for
-    // some reason (probably complexity) applying shadow to the
-    // Panel did not produce any shadows on the frame
-    $('<div>')
-      .css({
-        ...this.panelCommonCss,
-        width: '100%',
-        height: '100%',
+        borderRadius: '0 0 var(--border-radius) var(--border-radius)',
         clipPath: `polygon(
           0 var(--y-offset-calc),
           calc(var(--logo-unused-width-half) - var(--logo-offset-2x)) var(--y-offset-calc),
@@ -167,12 +106,55 @@ export default class Panel {
           0 var(--y-offset-calc)
         )`,
       })
-      .appendTo($wrapper);
+      .append($logo);
+  }
 
-    $wrapper.prependTo(this.$el);
+  // for masking, was only able to get default alpha to work. take black/white
+  // image into gimp, create a layer mask from a greyscale copy of the image
+  // (option when creating the mask) and then check "invert". the shorthand
+  // `mask` property didn't work either, only webkit-prefixed props
+  $getFrameLogo() {
+    return $('<img>')
+      .addClass('Panel--frame-logo')
+      .attr('src', './img/primary2.png')
+      .css({
+        boxSizing: 'border-box',
+        position: 'absolute',
+        bottom: this.logoOffset,
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: this.logoWidth,
+        '-webkit-mask-image': 'url("./img/mask_rust_alpha_black.png")',
+        '-webkit-mask-size': '100%',
+        filter: 'drop-shadow(0px 0px 3px #000) brightness(0.9)',
+      });
+  }
+
+  // clipped element needs a wrapper to apply drop shadow.
+  // to avoid a super complicated clip path, the frame is
+  // broken into 2 parts. given the background image, the
+  // seam is unnoticeable.
+  $getPanelFrame() {
+    const $frameTop = this.$getFrameTop();
+    const $frameBottom = this.$getFrameBottom();
+
+    const $wrapper = $('<div>')
+      .addClass('Panel--frame-wrapper')
+      .css({
+        boxSizing: 'border-box',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        filter: 'var(--drop-shadow)',
+      });
+
+    return $wrapper.append($frameTop, $frameBottom);
   }
 
   addContent(content) {
-    this.$el.append(content);
+    // prepend to keep content under frame (and therefore shadow)
+    this.$el.prepend(content);
   }
 }
